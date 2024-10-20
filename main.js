31d73e05fde235fb6cab28af4a51aa50d0636daa
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-20 22:11:02 +0200

    for - commit

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 //Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
-const emailList = document.getElementById('email-list'); //collego js all'elemento html in cui vorrei salvare la lista di email
+const emailList = document.getElementById('email-list'); ////collego js all'elemento html in cui vorrei salvare la lista di email
                                                                   
-function generateEmails() {//creo una funzione per ottenere 10 email
-    
+function generateEmails() { ////creo una funzione per ottenere 10 email
+
+        
+        for (let i = 0; i < 10; i++) {  ////creo un ciclo per eseguire le 10 richieste 
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => { 
