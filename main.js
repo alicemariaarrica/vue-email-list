bcf3be397cd051f57f1a52b27686fa66cff889fe
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-21 20:29:35 +0200

    var-email - commit

--- main.js
+++ main.js
@@ -2,10 +2,14 @@
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
 const emailList = document.getElementById('email-list'); ////collego js all'elemento html in cui vorrei salvare la lista di email
-                                                                  
+
 function generateEmails() { ////creo una funzione per ottenere 10 email
 
-        
-        for (let i = 0; i < 10; i++) {  ////creo un ciclo per eseguire le 10 richieste 
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(response => { 
+
+    for (let i = 0; i < 10; i++) {  ////creo un ciclo per eseguire le 10 richieste HTTPS
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+
+                const email = response.data.response; //// creo una variabile in cui salvare l'email dalla risposta dell'API 
+                //che cambia ad ogni iterazione
+
